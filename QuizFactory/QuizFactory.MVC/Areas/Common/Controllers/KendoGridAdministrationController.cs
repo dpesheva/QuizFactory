166aea1c3c53ea16a6836d9cf010bedb78c89384
dpesheva <dkshosa@gmail.com>
2014-11-14 11:48:54 +0200

    Add KendoGridAdministrationController
    + remove useless views

---
+++ QuizFactory/QuizFactory.MVC/Areas/Common/Controllers/KendoGridAdministrationController.cs
@@ -0,0 +1,76 @@
+﻿namespace QuizFactory.Mvc.Areas.Common.Controllers
+{
+    using System.Collections;
+    using System.Data.Entity;
+    using System.Web.Mvc;
+    using AutoMapper;
+    using Kendo.Mvc.Extensions;
+    using Kendo.Mvc.UI;
+    using QuizFactory.Data;
+    using QuizFactory.Data.Common;
+    using QuizFactory.Mvc.Areas.Users.ViewModels;
+    using QuizFactory.Mvc.Controllers;
+
+    public abstract class KendoGridAdministrationController : BaseController
+    {
+        public KendoGridAdministrationController(IQuizFactoryData data)
+            : base(data)
+        {
+        }
+
+        [HttpPost]
+        public ActionResult Read([DataSourceRequest]
+                                 DataSourceRequest request)
+        {
+            var ads =
+                this.GetData()
+                    .ToDataSourceResult(request);
+
+            return this.Json(ads);
+        }
+
+        protected abstract IEnumerable GetData();
+
+        protected abstract T GetById<T>(object id) where T : class;
+
+        [NonAction]
+        protected virtual T Create<T>(object model) where T : class
+        {
+            if (model != null && this.ModelState.IsValid)
+            {
+                var dbModel = Mapper.Map<T>(model);
+                this.ChangeEntityStateAndSave(dbModel, EntityState.Added);
+                return dbModel;
+            }
+
+            return null;
+        }
+
+        [NonAction]
+        protected virtual void Update<TModel, TViewModel>(TViewModel model, object id)
+            where TModel : AuditInfo
+            where TViewModel : QuizViewModel
+        {
+            if (model != null && this.ModelState.IsValid)
+            {
+                var dbModel = this.GetById<TModel>(id);
+                Mapper.Map<TViewModel, TModel>(model, dbModel);
+                this.ChangeEntityStateAndSave(dbModel, EntityState.Modified);
+                // model.ModifiedOn = dbModel.ModifiedOn;
+            }
+        }
+
+        protected JsonResult GridOperation<T>(T model, [DataSourceRequest]
+                                              DataSourceRequest request)
+        {
+            return this.Json(new[] { model }.ToDataSourceResult(request, ModelState));
+        }
+
+        private void ChangeEntityStateAndSave(object dbModel, EntityState state)
+        {
+            var entry = this.db.Context.Entry(dbModel);
+            entry.State = state;
+            this.db.SaveChanges();
+        }
+    }
+}
