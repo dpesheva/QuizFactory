f6024ca8c0657cab4eeda90ef9225f66654fd6be
dpesheva <dkshosa@gmail.com>
2015-01-30 18:53:56 +0200

    disable automatic migration data loss

--- QuizFactory/QuizFactory.Data/Migrations/Configuration.cs
+++ QuizFactory/QuizFactory.Data/Migrations/Configuration.cs
@@ -10,7 +10,7 @@ namespace QuizFactory.Data.Migrations
         public Configuration()
         {
             this.AutomaticMigrationsEnabled = true;
-            this.AutomaticMigrationDataLossAllowed = true;
+            this.AutomaticMigrationDataLossAllowed = false;
         }
 
         protected override void Seed(QuizFactoryDbContext context)
